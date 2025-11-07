0b2b91db294fafe25d77de0937d9100fcff16ce2
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-07 09:32:06 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ihor Sukhachov on 07.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
