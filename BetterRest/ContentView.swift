1f45c8b44b78dfc959391cea29914639de271b08
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-07 09:57:34 +0200

    added dates range

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,6 +14,13 @@ struct ContentView: View {
         Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
         DatePicker("Choose a date", selection: $wakeUp).labelsHidden()
     }
+    func exampleDates() {
+        // create a second Date instance set to one day in seconds from now
+        let tomorrow = Date.now.addingTimeInterval(86400)
+
+        // create a range from those two
+        let range = Date.now...tomorrow
+    }
 }
 
 #Preview {
