c2ab5e3c567832f8edd1699fb4d26997103ed4c0
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-07 09:53:08 +0200

    hided the date label

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     @State private var wakeUp = Date.now
     var body: some View {
         Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        DatePicker("Choose a date", selection: $wakeUp)
+        DatePicker("Choose a date", selection: $wakeUp).labelsHidden()
     }
 }
 
