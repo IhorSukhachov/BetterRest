429ebdc59a0f66a8ffbf63a8ca144c3b9410e942
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-08 13:41:55 +0200

    try\catch for caluclate bedtime

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Ihor Sukhachov on 07.11.2025.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
@@ -37,7 +37,13 @@ struct ContentView: View {
     }
  
     func calculateBedtime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+        }
+        catch {
+            
+        }
     }
     }
 
