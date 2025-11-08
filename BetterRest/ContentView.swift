5ec31a421d0d5b7cc1abeddd9d896ee34d407a29
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-08 12:44:27 +0200

    added time picker for wake up

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var coffeeAmount = 1
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-        DatePicker("Choose a date", selection: $wakeUp).labelsHidden()
-        Text(Date.now, format: .dateTime.day().month().year())
+        NavigationStack {
+            VStack {
+                Text("When do you want to wake up?").font(.headline)
+                DatePicker("Please enter time:", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
+            }
+        }
     }
-    func exampleDates() {
-       /* var components = DateComponents()
-        components.hour = 8
-        components.minute = 0
-        let date = Calendar.current.date(from: components) ?? .now*/
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
+ 
     }
-}
+
 
 #Preview {
     ContentView()
