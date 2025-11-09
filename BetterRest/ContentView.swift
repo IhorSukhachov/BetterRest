df024c2bc37f242cd5ccc501dab30c6a87a262fc
IhorSukhachov <i.sukhachov@gmail.com>
2025-11-09 09:00:38 +0200

    removed cacl button and added dynamic optimal bedtime change

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var alertTitle = ""
     @State private var alertMessage = ""
-    @State private var showingAlert = false
+  //  @State private var showingAlert = false
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -22,6 +22,27 @@ struct ContentView: View {
         components.minute = 0
         return Calendar.current.date(from: components) ?? .now
         
+        
+    }
+    var idealBedtime: String {
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+          //  alertTitle = "Your ideal bedtime is..."
+          //  alertMessage = "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+            return "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+        }
+        catch {
+          //  alertTitle = "Error"
+          //  alertMessage = "Sorry, there was a problem calculating your bedtime. Please try again."
+            return "There is an error occured"
+        }
+    
         
     }
     
@@ -51,20 +72,32 @@ struct ContentView: View {
                             Text($0, format: .number)
                         }
                     }
+                    
+                   
                 }
-              } 
+              
+                VStack(alignment: .leading) {
+                    Text("Your optimal bedtime is:").font(.headline).padding(.vertical).frame(maxWidth: .infinity, alignment: .center)
+                    Text(idealBedtime).font(.largeTitle.bold())
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(.vertical)
+                }
+             //   VStack(alignment: .leading, spacing: 0) {
+                    
+            //    }
+              }
             .navigationTitle("Better Rest")
-            .toolbar {
+           /* .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }.alert(alertTitle, isPresented: $showingAlert) {
                 Button("Ok"){ }
             } message: {
                 Text(alertMessage)
-            }
+            } */
         }
     }
     
-    func calculateBedtime() {
+   /* func calculateBedtime() {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -80,8 +113,8 @@ struct ContentView: View {
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime. Please try again."
         }
-        showingAlert = true
-    }
+       // showingAlert = true
+    } */
 }
 
 
